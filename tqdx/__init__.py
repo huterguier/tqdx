70fca191a0d59481be29743b3a3817b0616bf650
huterguier <huterguier@gmail.com>
2025-06-06 11:23:15 +0200

    fixed relative imports

--- tqdx/__init__.py
+++ tqdx/__init__.py
@@ -1,2 +1,2 @@
-from callbacks import init_pbar, update_pbar, close_pbar
-from wrappers import scan, fori_loop, tqdm
+from .callbacks import init_pbar, update_pbar, close_pbar
+from .wrappers import scan, fori_loop, tqdm
